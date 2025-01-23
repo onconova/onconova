b1d582e2736e0e0d24d83e4e2421b0bbc9d56eb3
Luis Fábregas-Ibáñez <luisfabib@gmail.com>
2025-01-23 09:13:46 +0100

    feat(pop-web): add disclaimer to the dashboard

--- pop-web/src/app/features/dashboard/dashboard.component.ts
+++ pop-web/src/app/features/dashboard/dashboard.component.ts
@@ -1,16 +1,25 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewEncapsulation } from '@angular/core';
 import { AuthService } from 'src/app/core/auth/services/auth.service';
 import { InlineSVGModule } from 'ng-inline-svg-2';
+
+import { DialogModule } from 'primeng/dialog';
+import { Button } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
     standalone: true,
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.css',
+    encapsulation: ViewEncapsulation.None,
     imports: [
-        InlineSVGModule
+        InlineSVGModule,
+        CardModule,
+        Button,
+        DialogModule
     ],
 })
 export class DashboardComponent {
     public readonly authService = inject(AuthService);
     public readonly illustration: string = 'assets/images/landing/researcher.svg';
+    public disclaimerDialogVisible: boolean = false;
 }
