41caf400280a080a6f5f4f4847009d7467139c27
Luis Fábregas-Ibáñez <luisfabib@gmail.com>
2025-02-02 13:33:38 +0100

    fix(pop-server); minor bug in the vitals schema

--- pop-server/pop/oncology/schemas/Vitals.py
+++ pop-server/pop/oncology/schemas/Vitals.py
@@ -1,6 +1,7 @@
 from pop.oncology.models import Vitals
 from pop.core.schemas import CREATE_IGNORED_FIELDS, create_schema, GetMixin, CreateMixin, ConfigDict
 from pop.core.measures import MeasureSchema
+from typing import Optional
 from ninja import Schema, Field
 
 VitalsBase: Schema = create_schema(
@@ -9,7 +10,7 @@ VitalsBase: Schema = create_schema(
 )
 
 class VitalsSchema(VitalsBase, GetMixin):
-    body_mass_index: MeasureSchema = Field(description='Bodymass index of the patient')
+    body_mass_index: Optional[MeasureSchema] = Field(None, description='Bodymass index of the patient')
     model_config = ConfigDict(title='Vitals')
     
 class VitalsCreateSchema(VitalsBase, CreateMixin):
