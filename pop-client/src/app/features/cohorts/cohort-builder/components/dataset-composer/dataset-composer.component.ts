d80d128704017c0a3163286cc9663a08972ccbb4
Luis Fábregas-Ibáñez <luisfabib@gmail.com>
2025-03-27 16:12:03 +0100

    chore: re-organize files

--- pop-client/src/app/features/cohorts/cohort-builder/components/dataset-composer/dataset-composer.component.ts
+++ pop-client/src/app/features/cohorts/cohort-builder/components/dataset-composer/dataset-composer.component.ts
@@ -19,7 +19,7 @@ import { TreeModule} from 'primeng/tree';
 import { AutoComplete } from 'primeng/autocomplete';
 
 
-import openApiSchema from "../../../../../openapi.json";
+import openApiSchema from "../../../../../../../openapi.json";
 import { CohortsService, Dataset, DatasetsService, PaginatedDataset, DataResource } from "src/app/shared/openapi";
 import { NestedTableComponent } from "src/app/shared/components";
 import { NgxJdenticonModule } from "ngx-jdenticon";
