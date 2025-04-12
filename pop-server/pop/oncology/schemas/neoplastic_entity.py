e6f72c122b0672282c1bb1506aca717d8817292d
Luis Fábregas-Ibáñez <luisfabib@gmail.com>
2025-04-12 20:56:21 +0200

    fix: minor bug in neoplastic entity schema

--- pop-server/pop/oncology/schemas/neoplastic_entity.py
+++ pop-server/pop/oncology/schemas/neoplastic_entity.py
@@ -1,4 +1,5 @@
 from pop.oncology import models as orm
+from typing import Optional
 from pop.core.schemas.factory import ModelGetSchema, ModelCreateSchema, SchemaConfig
 from pop.core.schemas import CodedConceptSchema
 from pydantic import Field, AliasChoices 
@@ -6,7 +7,8 @@ from pydantic import Field, AliasChoices
 
 class NeoplasticEntitySchema(ModelGetSchema):
 
-    topographyGroup: CodedConceptSchema = Field(
+    topographyGroup: Optional[CodedConceptSchema] = Field(
+        None,
         title='Topographical group', 
         alias='topography_group', 
         description='Broad anatomical location of the neoplastic entity',
