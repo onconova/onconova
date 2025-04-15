f90e34e24548710405457faf4f85d104f0b10215
Luis Fábregas-Ibáñez <luisfabib@gmail.com>
2025-04-15 07:53:28 +0200

    fix: do not confound integers with date-years in type checker

--- pop-client/src/app/shared/services/type-check.service.ts
+++ pop-client/src/app/shared/services/type-check.service.ts
@@ -13,7 +13,7 @@ export class TypeCheckService {
     public readonly isMeasure = (value: Measure): value is Measure => !!value?.value;
     public readonly isObject = (x: any) => typeof x === 'object' && !Array.isArray(x) && x !== null
     public readonly isArray = (x: any) => x instanceof Array
-    public readonly isDateString = (value: any): boolean => typeof value === 'string' && !isNaN(Date.parse(value));
+    public readonly isDateString = (value: any): boolean => typeof value === 'string' && !isNaN(Date.parse(value)) && value.length === 10;
     public readonly isDate = (value: any): value is Date => Object.prototype.toString.call(value) === '[object Date]' && !isNaN(value.getTime());
     public readonly isUUID = (value: any): boolean => /^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i.test(value);
 }
