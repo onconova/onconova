f7b36ce336df1850e2ec88fd33fc1bbb6fe59719
Luis Fábregas-Ibáñez <luisfabib@gmail.com>
2025-05-19 14:46:46 +0200

    refactor: simplify sidebar menu and disable routes for external users

--- pop-client/src/app/core/layout/components/app.sidebar-menu.component.ts
+++ pop-client/src/app/core/layout/components/app.sidebar-menu.component.ts
@@ -1,26 +1,20 @@
 import { Component, computed, ElementRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppMenuitemComponent } from './app.menuitem.component';
 import { BASE_PATH } from 'src/app/shared/openapi';
 import { AuthService } from 'src/app/core/auth/services/auth.service';
 import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
     selector: 'pop-sidebar-menu',
     template: `
-        <ul class="layout-menu">
-            @for (item of navigationMenuItems(); track item.label; let i = $index;) {
-                @if (item.separator) {
-                    <li class="menu-separator"></li>
-                } @else {
-                    <li pop-menuitem [item]="item" [index]="i" [root]="true"></li>
-                }
-            }
-        </ul>
+        <div class="pop-layout-sidebar">
+            <p-menu [model]="navigationMenuItems()" />
+        </div>
     `,
     imports: [
+        MenuModule,
         CommonModule,
-        AppMenuitemComponent
     ]
 })
 export class AppSidebarMenuComponent {
@@ -30,35 +24,75 @@ export class AppSidebarMenuComponent {
     readonly #basePath: string = inject(BASE_PATH);
     public el = inject(ElementRef);
 
+    #accessLevel = computed(() => this.#authService.user().accessLevel || 0); 
+
     private readonly currenUser = computed(() => this.#authService.user());
     public readonly navigationMenuItems = computed<MenuItem[]>(() => {
         let items: MenuItem[] = [
             {
                 label: 'Home',
                 items: [
-                    { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/dashboard'] }
+                    { 
+                        label: 'Dashboard', 
+                        icon: 'pi pi-fw pi-home', 
+                        routerLink: ['/dashboard'], 
+                    }
                 ]
             },
             {
                 label: 'Cases',
                 items: [
-                    { label: 'Search Cases', icon: 'pi pi-fw pi-search', routerLink: ['/cases/search'] },
-                    { label: 'My Cases', icon: 'pi pi-fw pi-search-plus', routerLink: ['/cases/search/', this.currenUser().username] },
-                    { label: 'Import', icon: 'pi pi-fw pi-file-import', routerLink: ['/cases/import'] },
+                    { 
+                        label: 'Search Cases', 
+                        icon: 'pi pi-fw pi-search', 
+                        routerLink: ['/cases/search'],
+                        disabled: this.#accessLevel() == 0,
+                    },
+                    { 
+                        label: 'My Cases', 
+                        icon: 'pi pi-fw pi-search-plus', 
+                        routerLink: ['/cases/search/', this.currenUser().username],
+                        disabled: this.#accessLevel() == 0,
+                    },
+                    { 
+                        label: 'Import', 
+                        icon: 'pi pi-fw pi-file-import', 
+                        routerLink: ['/cases/import'],
+                        disabled: this.#accessLevel() == 0,
+                    },
                 ]
             },
             {
                 label: 'Cohorts',
                 items: [
-                    { label: 'Search Cohorts', icon: 'pi pi-fw pi-users', routerLink: ['/cohorts/search'] },
-                    { label: 'My Cohorts', icon: 'pi pi-fw pi-user', routerLink: ['/cohorts/search/', this.currenUser().username] },
+                    { 
+                        label: 'Search Cohorts', 
+                        icon: 'pi pi-fw pi-users', 
+                        routerLink: ['/cohorts/search'],
+                        disabled: this.#accessLevel() == 0,
+                    },
+                    { 
+                        label: 'My Cohorts', 
+                        icon: 'pi pi-fw pi-user', 
+                        routerLink: ['/cohorts/search/', this.currenUser().username],
+                        disabled: this.#accessLevel() == 0,
+                    },
                 ]
             },
             {
                 label: 'Documentation',
                 items: [
-                    { label: 'User manual', icon: 'pi pi-fw pi-book', url: `/notfound`},
-                    { label: 'API Specification', icon: 'pi pi-fw pi-book', url: `${this.#basePath}/api/docs#/`},
+                    { 
+                        label: 'User manual', 
+                        icon: 'pi pi-fw pi-book', 
+                        url: `/notfound`,
+                        disabled: true
+                    },
+                    { 
+                        label: 'API Specification', 
+                        icon: 'pi pi-fw pi-book', 
+                        url: `${this.#basePath}/api/docs#/`,
+                    },
                 ]
             }
         ];
