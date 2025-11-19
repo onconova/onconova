19d3d424fd928c02cf414164cadc59030a32295f
Luis Fábregas-Ibáñez <luisfabib@gmail.com>
2025-11-19 10:40:40 +0100

    test: add FHIR CRUD API controller tests for Patient resource

---
+++ server/onconova/tests/interoperability/test_fhir_controllers.py
@@ -0,0 +1,183 @@
+from django.test import TestCase
+from parameterized import parameterized
+
+from onconova.oncology import models, schemas
+from onconova.interoperability.fhir import schemas
+from onconova.tests import common, factories
+from onconova.tests.common import (
+    GET_HTTP_SCENARIOS,
+    HTTP_SCENARIOS,
+    ApiControllerTestMixin,
+)
+from typing import List, Type
+from factory.django import DjangoModelFactory
+from ninja import Schema
+import pghistory
+
+from onconova.core.models import BaseModel
+
+
+class FhirCrudApiControllerTestCase(ApiControllerTestMixin, TestCase):
+
+    # Public interface
+    FACTORY: type[DjangoModelFactory] | List[type[DjangoModelFactory]]
+    factories: List[type[DjangoModelFactory]]
+    MODEL: Type[BaseModel] | List[Type[BaseModel]]
+    SCHEMA: Type[Schema]
+
+    # Internal state
+    models: List[Type[BaseModel]]
+    schemas: List[Type[Schema]]
+    create_schemas: List[Type[Schema]]
+
+    __test__ = False  # Prevent pytest from collecting this base class as a test
+
+    @classmethod
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        # Ensure subclasses are collected as tests
+        cls.__test__ = True
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        # Ensure class settings are iterable
+        cls.factories = (
+            [cls.FACTORY] if not isinstance(cls.FACTORY, list) else cls.FACTORY
+        )
+        cls.subtests = len(cls.factories)
+        cls.models = (
+            [cls.MODEL] * cls.subtests if not isinstance(cls.MODEL, list) else cls.MODEL
+        )
+        cls.schemas = (
+            [cls.SCHEMA] * cls.subtests
+            if not isinstance(cls.SCHEMA, list)
+            else cls.SCHEMA
+        )
+        cls.instances = []
+        cls.create_payloads = []
+        cls.update_payloads = []
+
+    @parameterized.expand(GET_HTTP_SCENARIOS)
+    def test_read_operation(self, scenario, config):
+        for i, (instance, schema, model) in enumerate(
+            zip(self.instances, self.schemas, self.models)
+        ):
+            # Call the API endpoint
+            response = self.call_api_endpoint(
+                "GET", self.get_route_url_with_id(instance), anonymized=False, **config
+            )
+            with self.subTest(i=i):
+                # Assert response content
+                if scenario == "HTTPS Authenticated":
+                    self.assertEqual(response.status_code, 200)
+                    expected = schema.model_validate(instance).model_dump()
+                    result = schema.model_validate(response.json()).model_dump()
+                    self.assertEqual(
+                        result,
+                        expected,
+                        f"Response FHIR data does not match expected for {model.__name__}",
+                    )
+
+                model.objects.all().delete()
+
+    @parameterized.expand(HTTP_SCENARIOS)
+    def test_delete_operation(self, scenario, config):
+        for i, (instance, schema, model) in enumerate(
+            zip(self.instances, self.schemas, self.models)
+        ):
+            # Call the API endpoint
+            response = self.call_api_endpoint(
+                "DELETE", self.get_route_url_with_id(instance), **config
+            )
+            with self.subTest(i=i):
+                # Assert response content
+                if scenario == "HTTPS Authenticated":
+                    self.assertEqual(response.status_code, 204)
+                    self.assertFalse(model.objects.filter(id=instance.id).exists())
+                    # Assert audit trail
+                    self.assertTrue(
+                        pghistory.models.Events.objects.filter(  # type: ignore
+                            pgh_obj_id=instance.id, pgh_label="delete"
+                        ).exists(),
+                        "Event not properly registered",
+                    )
+                model.objects.all().delete()
+
+    @parameterized.expand(HTTP_SCENARIOS)
+    def test_create_operation(self, scenario, config):
+        for i, (instance, payload, model) in enumerate(
+            zip(self.instances, self.create_payloads, self.models)
+        ):
+            instance.delete()
+            # Call the API endpoint.
+            response = self.call_api_endpoint(
+                "POST", self.get_route_url(instance), data=payload, **config
+            )
+            with self.subTest(i=i):
+                # Assert response content
+                if scenario == "HTTPS Authenticated":
+                    created_id = response.json()["id"]
+                    created_instance = model.objects.filter(id=created_id).first()
+                    assert created_instance is not None, "Resource has not been created"
+                    # Assert audit trail
+                    self.assertEqual(
+                        self.user.username,
+                        created_instance.created_by,
+                        "Unexpected creator user.",
+                    )
+                    self.assertTrue(
+                        created_instance.events.filter(pgh_label="create").exists(),  # type: ignore
+                        "Event not properly registered",
+                    )
+                model.objects.all().delete()
+
+    @parameterized.expand(HTTP_SCENARIOS)
+    def test_update_operation(self, scenario, config):
+        for i, (instance, payload, model) in enumerate(
+            zip(self.instances, self.update_payloads, self.models)
+        ):
+            with self.subTest(i=i):
+                # Call the API endpoint
+                response = self.call_api_endpoint(
+                    "PUT", self.get_route_url_with_id(instance), data=payload, **config
+                )
+                # Assert response content
+                if scenario == "HTTPS Authenticated":
+                    updated_id = response.json()["id"]
+                    self.assertEqual(response.status_code, 200)
+                    updated_instance = model.objects.filter(id=updated_id).first()
+                    assert (
+                        updated_instance is not None
+                    ), "The updated instance does not exist"
+                    self.assertNotEqual(
+                        [
+                            getattr(instance, field.name)
+                            for field in model._meta.concrete_fields
+                        ],
+                        [
+                            getattr(updated_instance, field.name)
+                            for field in model._meta.concrete_fields
+                        ],
+                    )
+                    # Assert audit trail
+                    if updated_instance.updated_by:
+                        self.assertIn(
+                            self.user.username,
+                            updated_instance.updated_by,  # type: ignore
+                            "The updating user is not registered",
+                        )
+                    self.assertTrue(
+                        pghistory.models.Events.objects.filter(  # type: ignore
+                            pgh_obj_id=instance.id, pgh_label="update"
+                        ).exists(),
+                        "Event not properly registered",
+                    )
+                model.objects.all().delete()
+
+
+class TestPatientCaseController(FhirCrudApiControllerTestCase):
+    controller_path = "/api/fhir/Patient/"
+    FACTORY = factories.PatientCaseFactory
+    MODEL = models.PatientCase
+    SCHEMA = schemas.OnconovaCancerPatient
