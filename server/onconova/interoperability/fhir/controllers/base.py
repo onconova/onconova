f21e0c6299c3dd1dfcb6b55e56fc09f35a75010a
Luis Fábregas-Ibáñez <luisfabib@gmail.com>
2025-11-19 10:09:30 +0100

    implement FHIR resource CRUD operations in FhirBaseController

---
+++ server/onconova/interoperability/fhir/controllers/base.py
@@ -0,0 +1,159 @@
+from django.db import IntegrityError, models
+from ninja_extra import ControllerBase, api_controller, route
+from onconova.core.utils import COMMON_HTTP_ERRORS
+from onconova.interoperability.fhir.schemas import OnconovaCancerPatient
+from onconova.oncology.models import PatientCase
+from fhircraft.fhir.resources.datatypes.R4.resources.operation_outcome import (
+    OperationOutcome,
+    OperationOutcomeIssue,
+)
+from fhircraft.fhir.resources.base import FHIRBaseModel
+from pydantic_core import ValidationError
+
+COMMON_READ_HTTP_ERRORS = {410: None, 404: None}
+COMMON_UPDATE_HTTP_ERRORS = {
+    400: OperationOutcome,
+    405: OperationOutcome,
+    409: OperationOutcome,
+    402: None,
+}
+COMMON_DELETE_HTTP_ERRORS = {404: OperationOutcome}
+COMMON_CREATE_HTTP_ERRORS = {
+    400: OperationOutcome,
+    409: OperationOutcome,
+}
+
+
+class FhirBaseController(ControllerBase):
+
+    def read_fhir_resource(self, rid: str, model: type[models.Model]):
+        """Read a FHIR resource by its ID"""
+        # Get the instance from the database
+        instance = model.objects.filter(id=rid).first()
+        if not instance:
+            # If resource not found, check if it was deleted
+            if model.pgh_event_model.objects.filter(pgh_obj_id=rid, pgh_label="delete").exists():  # type: ignore
+                return 410, None
+            return 404, None
+        # Set the Last-Modified header if the instance has an updated_at timestamp
+        if getattr(instance, "updated_at", None):
+            assert self.context and self.context.response
+            self.context.response.headers["Last-Modified"] = instance.updated_at.isoformat()  # type: ignore
+        return instance
+
+    def update_fhir_resource(self, rid: str, payload: FHIRBaseModel):
+        """Update a FHIR resource by its ID"""
+        assert self.context and self.context.response and self.context.request
+        instance = PatientCase.objects.filter(id=rid).first()
+        if not instance:
+            return 405, OperationOutcome(
+                issue=[
+                    OperationOutcomeIssue(
+                        severity="error",
+                        code="not-found",
+                        diagnostics="Resource with given ID not found, and create-update is not supported.",
+                    )
+                ]
+            )
+        if instance and str(instance.id) != payload.id:  # type: ignore
+            return 400, OperationOutcome(
+                issue=[
+                    OperationOutcomeIssue(
+                        severity="error",
+                        code="invalid",
+                        diagnostics="ID in payload does not match ID in URL.",
+                    )
+                ]
+            )
+        try:
+            resource = payload.__class__.fhir_to_onconova(  # type: ignore
+                payload
+            ).model_dump_django(instance=instance)
+        except ValidationError as ve:
+            return 400, OperationOutcome(
+                issue=[
+                    OperationOutcomeIssue(
+                        severity="error",
+                        code="invalid",
+                        diagnostics=str(ve),
+                    )
+                ]
+            )
+        except IntegrityError as ie:
+            return 409, OperationOutcome(
+                issue=[
+                    OperationOutcomeIssue(
+                        severity="error",
+                        code="conflict",
+                        diagnostics="One of the unique fields conflicts with an existing record.",
+                    )
+                ]
+            )
+        self.context.response.headers["Location"] = f"/fhir/Patient/{resource.id}"
+        preference = self.context.request.headers.get("Prefer", "return=representation")
+        if preference == "return=representation":
+            return 200, resource
+        elif preference == "return=minimal":
+            return 200, None
+        elif preference == "return=OperationOutcome":
+            return 200, OperationOutcome(
+                issue=[OperationOutcomeIssue(severity="success")]
+            )
+        else:
+            return 400, None
+
+    def delete_fhir_resource(self, rid: str, model: type[models.Model]):
+        """Delete a FHIR resource by its ID"""
+        assert self.context and self.context.response and self.context.request
+        instance = model.objects.filter(id=rid).first()
+        if not instance:
+            return 404, OperationOutcome(
+                issue=[
+                    OperationOutcomeIssue(
+                        severity="error",
+                        code="not-found",
+                        diagnostics="Resource with given ID not found.",
+                    )
+                ]
+            )
+        instance.delete()
+        return 204, None
+
+    def create_fhir_resource(self, payload: FHIRBaseModel):
+        assert self.context and self.context.response and self.context.request
+        try:
+            resource = payload.__class__.fhir_to_onconova(  # type: ignore
+                payload
+            ).model_dump_django()
+        except ValidationError as ve:
+            return 400, OperationOutcome(
+                issue=[
+                    OperationOutcomeIssue(
+                        severity="error",
+                        code="invalid",
+                        diagnostics=str(ve),
+                    )
+                ]
+            )
+        except IntegrityError as ie:
+            return 409, OperationOutcome(
+                issue=[
+                    OperationOutcomeIssue(
+                        severity="error",
+                        code="conflict",
+                        diagnostics=str(ie),
+                    )
+                ]
+            )
+        self.context.response.headers["Location"] = f"/fhir/Patient/{resource.id}"
+        preference = self.context.request.headers.get("Prefer", "return=representation")
+        if preference == "return=representation":
+            return 200, resource
+        elif preference == "return=minimal":
+            return 200, None
+        elif preference == "return=OperationOutcome":
+            return 200, OperationOutcome(
+                issue=[OperationOutcomeIssue(severity="success")]
+            )
+        else:
+            return 400, None
