0ac89c118eddf26f397c312486a23a850c2d85e1
Luis Fábregas-Ibáñez <luisfabib@gmail.com>
2026-03-26 09:18:03 +0100

    feat: add user-friendly 422 HTTP error handling for database constraint violations

--- server/onconova/api.py
+++ server/onconova/api.py
@@ -4,6 +4,7 @@ standards-based interface for cancer genomics and clinical research data managem
 oncology, research, and interoperability controllers, and sets up OpenAPI documentation with custom settings and license information.
 """
 
+from django.db import IntegrityError
 from ninja import Redoc
 from ninja_extra import NinjaExtraAPI
 
@@ -150,3 +151,18 @@ api.register_controllers(
 @api.exception_handler(CodedConceptDoesNotExist)
 def terminology_exception_handler(request, exc):
     return api.create_response(request, {"message": str(exc)}, status=422)
+
+
+# # Handle database constraint violations as HTTP errors with a user-friendly message
+@api.exception_handler(IntegrityError)
+def database_exception_handler(request, exc):
+    return api.create_response(
+        request,
+        {
+            "message": "The data provided violates a database constraint: "
+            + str(exc.__cause__).split("\n")[
+                0
+            ]  # Include only the first line of the error message for clarity
+        },
+        status=422,
+    )
